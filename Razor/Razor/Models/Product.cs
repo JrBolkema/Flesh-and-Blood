6ef52060500723fb041c72b7ad97733ce6f9c96f
Jon Bolkema <jrbolkema@gmail.com>
2019-01-29 17:57:07 -0700

    halfway done

---
+++ Razor/Razor/Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor.Models
+{
+	public class Product
+	{
+		public int ProductID { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public decimal Price { get; set; }
+		public string Category { get; set; }
+
+		//public static Product[] GetProducts()
+		//{
+		//	Product kayak = new Product
+		//	{
+		//		Name = "Kayak",
+		//		Price = 275M
+		//	};
+
+		//	Product lifejacket = new Product
+		//	{
+		//		Name = "Lifejacket",
+		//		Price = 48.95M
+		//	};
+	
+		//	return new Product[] { kayak, lifejacket, null };
+
+		//}
+
+	}
+
+
+
+}
